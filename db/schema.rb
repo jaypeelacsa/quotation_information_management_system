5acd20158f727209378c2f7466daf064bf31d7b5
jaypeelacsa <jaypeelacsa011@gmail.com>
2016-03-17 10:33:27 +0800

    wala lang

--- db/schema.rb
+++ db/schema.rb
@@ -22,14 +22,6 @@ ActiveRecord::Schema.define(version: 20160316091309) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "check_particulars", force: :cascade do |t|
-    t.string   "particular"
-    t.decimal  "amount"
-    t.integer  "check_voucher_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "amount_in_word"
-    end
   create_table "billings", force: :cascade do |t|
     t.integer  "bill_no"
     t.date     "bill_date"
@@ -37,6 +29,15 @@ ActiveRecord::Schema.define(version: 20160316091309) do
     t.integer  "client_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "check_particulars", force: :cascade do |t|
+    t.string   "particular"
+    t.decimal  "amount"
+    t.integer  "check_voucher_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "amount_in_word"
   end
 
   create_table "check_vouchers", force: :cascade do |t|
@@ -54,9 +55,6 @@ ActiveRecord::Schema.define(version: 20160316091309) do
     t.string   "si_no"
     t.string   "or_no"
     t.string   "apv_no"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.string   "check_no"
   end
 
   create_table "clients", force: :cascade do |t|
