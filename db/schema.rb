a99cd99329b6b1fb897292c7a1d55cf86025b56c
jaypeelacsa <jaypeelacsa011@gmail.com>
2016-03-22 15:44:01 +0800

    design proposal

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160322044003) do
+ActiveRecord::Schema.define(version: 20160322063841) do
 
   create_table "accounting_codes", force: :cascade do |t|
     t.string   "name"
