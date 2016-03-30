c7462ea197402730c8512a2fd7989558d6340a5a
kaiservelilia <veliliakaizer12345@gmail.com>
2016-03-30 11:03:11 +0800

    active admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,9 @@
 class PagesController < ApplicationController
 	
-	# before_filter :authenticate_user!
-
-	# def index
-	# 	unless user_signed_in?
-	# 		redirect_to new_user_session_path
-	# 	end
-	# end
+	def index
+		unless user_signed_in?
+			redirect_to new_user_session_path
+		end
+	end
 
 end
