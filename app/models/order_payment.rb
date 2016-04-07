0170257b6b7238f8bdc6f36109606bf8cd87849b
kaiservelilia <veliliakaizer12345@gmail.com>
2016-04-07 13:01:22 +0800

    validates

--- app/models/order_payment.rb
+++ app/models/order_payment.rb
@@ -3,7 +3,7 @@ class OrderPayment < ActiveRecord::Base
 	validates :payment_no, presence: true
 	validates :payment_date, presence: true
 	validates :particular, presence: true
-	validates :paid_by, presence: true	
+	validates :currency, presence: true	
 	validates :amount, presence: true	
 
 	belongs_to :order
